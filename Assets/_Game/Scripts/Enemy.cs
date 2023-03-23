9b7881c791e1aa39e45e180143d2a7630b027b9b
EuDavidReis-ODev <eudavidreis.dev@gmail.com>
2023-03-23 15:37:56 -0300

    Destruindo inimigos usando Raycast2D, e mudando sprite.

--- Assets/_Game/Scripts/Enemy.cs
+++ Assets/_Game/Scripts/Enemy.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {   
+    private Animator enemyAnimator;
+
+    [SerializeField] private GameObject[] sprites;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyAnimator = GetComponent<Animator>();
+        sprites[0] = this.transform.GetChild(0).gameObject;
+        sprites[1] = this.transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         Moviment();
+        AnimationSpeed();
     }
 
     private void Moviment(){
         transform.Translate(Vector2.down * (speed * Time.deltaTime));
+    }
+
+    private void AnimationSpeed(){
+        enemyAnimator.SetFloat("Speed",speed);
+    }
+
+    public void Death(){
+        speed = 0f;
+        sprites[0].gameObject.SetActive(false);
+        sprites[1].gameObject.SetActive(true);
+        Destroy(this.gameObject,Random.Range(2.5f,5f));
     }
 }
---
+++ Assets/_Game/Scripts/TabDetector.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabDetector : MonoBehaviour
+{
+    private bool tapControl = false;
+
+    private Enemy enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectTap();
+    }
+
+    private void DetectTap(){
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position),Vector2.zero);
+            if(hit.collider != null){
+                tapControl = false;
+                TapObject(hit);
+            }
+        }
+    }
+
+    private void TapObject(RaycastHit2D hit){
+        if(hit.collider.gameObject.CompareTag("Enemy") && !tapControl){
+            tapControl = true;
+            hit.collider.gameObject.GetComponent<Enemy>().Death();
+        }
+    }
+}
